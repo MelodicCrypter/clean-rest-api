ec8b5eb3121f532ef97ea275413c1032f268a232
Hugh Caluscusin (Dev) <hugh@melodiccrypter.com>
2020-03-11 10:06:25 +0800

    added compression, util, modified

--- src/server.js
+++ src/server.js
@@ -8,6 +8,10 @@ import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
 import hpp from 'hpp';
+import compression from 'compression';
+
+// Util
+import shouldCompress from './util/shouldCompress';
 
 // Routes
 import router from './routes/routes';
@@ -21,9 +25,10 @@ const port = process.env.PORT || 3000;
 // Middlewares: Security
 app.use(cors({ origin: ['http://localhost:8000', /\.netlify\.com$/] })); // Cross-Origin
 app.use(helmet()); // Security for HTTP requests
+app.use(compression({ filter: shouldCompress, threshold: 0 })); // Compresses response
 app.use(express.json({ limit: '300kb' })); // Allows JSON but with limit
 app.use(hpp()); // Protection against Paramenter Pollution Attacks
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 
 // Load API routes
 app.use('/', router);
